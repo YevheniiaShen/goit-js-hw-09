f59439d028753457c14ffa6a3e45282c5ce532a7
YevheniiaShen <128001989+YevheniiaShen@users.noreply.github.com>
2023-12-08 12:07:13 +0100

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonOn = document.querySelector('[data-start]');
+const buttonOff = document.querySelector('[data-stop]');
 
+let intervalId;
+
+buttonOn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  buttonOn.disabled = true;
+  buttonOff.disabled = false;
+});
+
+buttonOff.addEventListener('click', () => {
+  clearInterval(intervalId);
+  buttonOn.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
