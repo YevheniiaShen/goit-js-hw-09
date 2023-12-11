9c62edd07cfd4fa6e06322702b6d5f8a3e18e8f5
YevheniiaShen <128001989+YevheniiaShen@users.noreply.github.com>
2023-12-11 15:16:39 +0000

    Deploying to gh-pages from @ YevheniiaShen/goit-js-hw-09@48062a76ec61456d922edae8344a287c698f42f4 🚀

--- 01-color-switcher.c8cf8251.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a;t.addEventListener("click",(()=>{a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.c8cf8251.js.map
